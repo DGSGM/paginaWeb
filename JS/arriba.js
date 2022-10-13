8f05647849496d02930a2877ca51796ab7ad6bc3
DGSGM <115727162+DGSGM@users.noreply.github.com>
2022-10-13 11:54:35 -0500

    Update arriba.js

--- JS/arriba.js
+++ JS/arriba.js
@@ -1,16 +1,16 @@
-(document).ready(function(){
+$(document).ready(function(){
     
-   ('.ir-arriba').click(function(){
-       ('body, html').animate({
+   $('.ir-arriba').click(function(){
+       $('body, html').animate({
            scrollTop: '0px'
        }, 300);
    });
     
-    (window).scroll(function(){
-       if((this).scrollTop()>0){
-            ('.ir-arriba').slideDown(300);
+    $(window).scroll(function(){
+       if($(this).scrollTop()>0){
+            $('.ir-arriba').slideDown(300);
         } else{
-            ('.ir-arriba').slideUp(300); 
+            $('.ir-arriba').slideUp(300); 
         } 
     });
 });
