0fc8889a8fb8434d9542fdbc0902ec3f176269b8
DGSGM <115727162+DGSGM@users.noreply.github.com>
2022-10-13 11:02:40 -0500

    Add files via upload

---
+++ JS/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5azBGNTJ6d_ENm6rYyuLNQaBAPTtEJKE",
+  authDomain: "movilidad-beded.firebaseapp.com",
+  projectId: "movilidad-beded",
+  storageBucket: "movilidad-beded.appspot.com",
+  messagingSenderId: "951318227154",
+  appId: "1:951318227154:web:d4daf0a02cbc702dd4a50b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
